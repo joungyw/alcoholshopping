aa0084807e34aed0641473eab290d437fb0ddd5d
DESKTOP-33SQ8HI\n <ceojoung@naver.com>
2024-04-25 17:38:37 +0900

    no message

--- src/main/java/com/three/alcoholshoppingmall/project/search/SearchController.java
+++ src/main/java/com/three/alcoholshoppingmall/project/search/SearchController.java
@@ -62,19 +62,27 @@ public class SearchController {
         List<Alcohol> list = alcoholRepository.findByMaincategory(alcoholDto.getMaincategory());
         return list;
     }
-
-    @PostMapping("/mycustom")
-    @Operation(summary = "대분류와 소분류가 db 내용과 일치 시 검색, 와인 소분류 검색시 띄어쓰기 유의해주세요.",
-            description = "    대분류와 소분류로 검색, 대분류: 와인- 소분류: 레드 와인, 화이트 와인, 스파클링 와인, 로제 와인\" +\n" +
-                    "            \"대분류: 위스키 - 소분류: 싱글몰트, 블렌디드, 버번\" +\n" +
-                    "            \"대분류: 브랜디 - 소분류: 꼬냑, 깔바도스, 아르마냑\" +\n" +
-                    "            \"대분류: 리큐르 = 소분류: 리큐르를 입력해주세요.")
-    public List<Alcohol> selectByMyCustom(@RequestBody AlcoholDto alcoholDto) {
-        List<Alcohol> list = alcoholRepository
-                .findByMyCustom(alcoholDto.getMaincategory(),
-                        alcoholDto.getSubcategory());
+    @PostMapping("/subcategory")
+    @Operation(summary = "소분류로 주류 검색",
+            description = "소분류로 주류를 검색합니다. 소분류에 해당하는 주류를 찾아줍니다")
+    public List<Alcohol> selectBySubCategory(@RequestBody AlcoholDto alcoholDto) {
+        List<Alcohol> list = alcoholRepository.findBySubcategory(alcoholDto.getSubcategory());
         return list;
     }
+
+
+//    @PostMapping("/mycustom")
+//    @Operation(summary = "대분류와 소분류가 db 내용과 일치 시 검색, 와인 소분류 검색시 띄어쓰기 유의해주세요.",
+//            description = "    대분류와 소분류로 검색, 대분류: 와인- 소분류: 레드 와인, 화이트 와인, 스파클링 와인, 로제 와인\" +\n" +
+//                    "            \"대분류: 위스키 - 소분류: 싱글몰트, 블렌디드, 버번\" +\n" +
+//                    "            \"대분류: 브랜디 - 소분류: 꼬냑, 깔바도스, 아르마냑\" +\n" +
+//                    "            \"대분류: 리큐르 = 소분류: 리큐르를 입력해주세요.")
+//    public List<Alcohol> selectByMyCustom(@RequestBody AlcoholDto alcoholDto) {
+//        List<Alcohol> list = alcoholRepository
+//                .findByMyCustom(alcoholDto.getMaincategory(),
+//                        alcoholDto.getSubcategory());
+//        return list;
+//    }
 
     @PostMapping("/name")
     @Operation(summary = "주류 이름으로 검색", description = "주류 이름으로 검색, 검색기록이 완료되지 않아도 주류들이 출력되게 만들었습니다.")
@@ -83,13 +91,14 @@ public class SearchController {
         return list;
     }
 
-    @PostMapping("/subcategory")
-    @Operation(summary = "대분류를 통해 소분류를 검색",
-            description = "대분류를 검색하면 소분류가 출력됩니다, 대분류는 와인, 위스키, 브랜디, 리큐르가 있습니다.")
-    public List<String> selectSubcategory(@RequestBody AlcoholDto alcoholDto) {
-        List<String> list = alcoholRepository.findSubcategoryByMaincategory(alcoholDto.getMaincategory());
-        return list;
-    }
-
+//
+//    @PostMapping("/subcategory")
+//    @Operation(summary = "대분류를 통해 소분류를 검색",
+//            description = "대분류를 검색하면 소분류가 출력됩니다, 대분류는 와인, 위스키, 브랜디, 리큐르가 있습니다.")
+//    public List<String> selectSubcategory(@RequestBody AlcoholDto alcoholDto) {
+//        List<String> list = alcoholRepository.findSubcategoryByMaincategory(alcoholDto.getMaincategory());
+//        return list;
+//    }
+//
 
 }
