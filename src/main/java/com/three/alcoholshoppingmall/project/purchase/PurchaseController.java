d8231d45552f20ebdcffb6403e94c2c44324fbd3
DESKTOP-33SQ8HI\n <ceojoung@naver.com>
2024-05-23 12:59:53 +0900

    yml 수정

--- src/main/java/com/three/alcoholshoppingmall/project/purchase/PurchaseController.java
+++ src/main/java/com/three/alcoholshoppingmall/project/purchase/PurchaseController.java
@@ -35,44 +35,31 @@ public class PurchaseController {
                     "입력 하실 값은 없습니다.")
     @SecurityRequirement(name = "bearerAuth")
     public ResponseEntity<List<Purchaseshow>> PICKUP() {
-
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        User user = (User)authentication.getPrincipal();
-
-
+        User user = (User) authentication.getPrincipal();
         List<Purchaseshow> list = purchaseServicce.PICKUPlist(user.getEmail());
         return ResponseEntity.status(HttpStatus.OK).body(list);
     }
-
     @GetMapping("/delivery")
     @Operation(summary = "구매내역 중 배달 보기",
             description = "회원의 구매 내역중 배달로 수령을 신청한 제품을 모두 보여줍니다." +
                     "입력 하실 값은 없습니다.")
     @SecurityRequirement(name = "bearerAuth")
     public ResponseEntity<List<Purchaseshow>> DELIVERY() {
-
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        User user = (User)authentication.getPrincipal();
-
-
+        User user = (User) authentication.getPrincipal();
         List<Purchaseshow> list = purchaseServicce.DELIVERYlist(user.getEmail());
-
         return ResponseEntity.status(HttpStatus.OK).body(list);
     }
-
     @GetMapping("/pickuplimt")
     @Operation(summary = "구매내역중 픽업 최근 5개만",
             description = "회원의 구매 내역중 가장 최근에 픽업으로 수령을 신청한 제품을 5개를 보여줍니다." +
                     "입력 하실 값은 없습니다.")
     @SecurityRequirement(name = "bearerAuth")
     public ResponseEntity<List<Purchaseshow>> PICKUPLIMT() {
-
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        User user = (User)authentication.getPrincipal();
-
-
+        User user = (User) authentication.getPrincipal();
         List<Purchaseshow> list = purchaseServicce.PICKUPlimt(user.getEmail());
-
         return ResponseEntity.status(HttpStatus.OK).body(list);
     }
 
@@ -82,13 +69,12 @@ public class PurchaseController {
                     "입력 하실 값은 없습니다.")
     @SecurityRequirement(name = "bearerAuth")
     public ResponseEntity<List<Purchaseshow>> DELIVERYLIMT() {
-
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        User user = (User)authentication.getPrincipal();
-
+        User user = (User) authentication.getPrincipal();
         List<Purchaseshow> list = purchaseServicce.DELIVERYLIMTlimt(user.getEmail());
         return ResponseEntity.status(HttpStatus.OK).body(list);
     }
+
     @GetMapping("/algorithm")
     @Operation(summary = "내 알고리즘",
             description = "회원의 구매정보를 토대로 술을 추천 합니다. 구매 정보가 없을 경우 많이 팔린 술 3개를 추천 합니다." +
@@ -100,10 +86,5 @@ public class PurchaseController {
         List<Alcoholmain> list = alcoholService.Algorithm(user.getEmail());
         return ResponseEntity.status(HttpStatus.OK).body(list);
     }
-
-
-
-
-
 }
 
