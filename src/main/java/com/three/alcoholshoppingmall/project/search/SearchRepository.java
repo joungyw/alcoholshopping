1d71998575f4eb6dc454dda0b76895ce20c2d4c8
DEVICE\k9315 <k93158612@gmail.com>
2024-06-05 16:00:33 +0900

    240605

--- src/main/java/com/three/alcoholshoppingmall/project/search/SearchRepository.java
+++ src/main/java/com/three/alcoholshoppingmall/project/search/SearchRepository.java
@@ -13,10 +13,10 @@ import java.util.List;
 
 @Repository
 public interface SearchRepository extends JpaRepository<Search, Long> {
-    @Query(value = "  SELECT DISTINCT searchcontents\n" +
+    @Query(value = "  SELECT DISTINCT searchcontents,id\n" +
             "FROM search\n" +
             "WHERE email = :email \n" +
-            "ORDER BY searchcontents DESC \n" +
+            "ORDER BY id DESC \n" +
             "LIMIT 5; ",nativeQuery = true)
     List<String> findAllByUserOrderByIdDesc(String email);
 
